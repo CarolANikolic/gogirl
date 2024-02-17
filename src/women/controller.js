0a4087b06c06f33bc858c90d7b9f1485c2147db7
Carol A. Nikolic <carolnikolic@gmail.com>
2024-02-17 19:13:57 +0100

    feat: send message for empty results for querying id, continent and name.

--- src/women/controller.js
+++ src/women/controller.js
@@ -15,7 +15,12 @@ const getWomanByID = async (req, res) => {
     try {
         const id = parseInt(req.params.id);
         const queryByIDResult = await db.query(queries.queryByID, [id]);
-        res.status(200).json(queryByIDResult.rows);
+
+        if (!queryByIDResult.rows.length) {
+            return res.send('No woman found with the given id.');
+        } 
+
+        res.status(200).json(queryByIDResult.rows); 
     } catch (error) {
         console.log('Error querying database by id:', error);
         res.status(500).json({ error: 'Internal server error' });
@@ -26,6 +31,11 @@ const getWomenByContinent = async (req, res) => {
     try {
         const continent = req.params.continent.toLowerCase();
         const queryByContinentResult = await db.query(queries.queryByContinent, [continent]);
+
+        if (!queryByContinentResult.rows.length) {
+            return res.send('No result found for the provided continent.');
+        }
+
         res.status(200).json(queryByContinentResult.rows);
     } catch (error) {
         console.log('Error querying database by continent:', error);
@@ -37,6 +47,10 @@ const getWomenByName = async (req, res) => {
     try {
         const name = req.params.name.toLowerCase();
         const queryByNameResult = await db.query(queries.queryByName, [name]);
+        if (!queryByNameResult.rows.length) {
+            return res.send('No woman found with this name.')
+        }
+        
         res.status(200).json(queryByNameResult.rows);
     } catch (error) {
         console.log('Error querying database by name:', error);
